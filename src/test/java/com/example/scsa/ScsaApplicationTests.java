8fc5f51d0c87c69488691975a92a3eefa4024348
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-11-15 16:22:01 +0900

    ➕Add: init project

---
+++ src/test/java/com/example/scsa/ScsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

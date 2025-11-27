32f184b8de8819f81b5241a226e4b08dfaeebdb5
Eric SHIM <163520887+Data-MaSTeRR@users.noreply.github.com>
2025-11-27 15:33:00 +0900

    🪄Fix: DTO에 생성자 롬복 추가

--- src/main/java/com/example/scsa/dto/court/CourtDTO.java
+++ src/main/java/com/example/scsa/dto/court/CourtDTO.java
@@ -1,11 +1,15 @@
 package com.example.scsa.dto.court;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CourtDTO {
     private Long courtId;
     private String thumbnail;
--- src/main/java/com/example/scsa/dto/profile/UserProfileDTO.java
+++ src/main/java/com/example/scsa/dto/profile/UserProfileDTO.java
@@ -1,10 +1,14 @@
 package com.example.scsa.dto.profile;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserProfileDTO {
 
     private Long userId;
